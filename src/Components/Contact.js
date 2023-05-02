d09a319a066825fa4146a3a4e6aecb381800c846
snehil891 <snehilsah400@gmail.com>
2023-05-03 02:32:05 +0530

    contact form designed

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default function Contact(props){
-    return(
+export default function Contact(props) {
+    return (
         <section className='contact' id='contact'>
             <h3 className='heading'>Contact<span>Me!</span></h3>
-            
+            <form>
+                <div className='input-box'>
+                    <div className='input-field'>
+                        <input type='text' placeholder='Full Name' required></input>
+                    </div>
+                    <div className='input-field'>
+                        <input type='text' placeholder='Email Address' required></input>
+                    </div>
+                </div>
+                <div className='input-box'>
+                    <div className='input-field'>
+                        <input type='tel' placeholder='Mobile Number' required></input>
+                    </div>
+                    <div className='input-field'>
+                        <input type='tel' placeholder='Subject' required></input>
+                    </div>
+                </div>
+                <div className='textarea'>
+                    <textarea name='' id='' cols={20} rows={10} placeholder='Your Question' required></textarea>
+                </div>
+                <div className='btn-box'>
+                    <button type='submit' className='btn'>Submit</button>
+                </div>
+            </form>
         </section>
     )
 }
 
-Contact.propTypes={
+Contact.propTypes = {
     title: PropTypes.string
 }
